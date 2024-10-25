1b6be8f2e25a74cbf32c92f9c7c64c456fd5a6ff
AlgoRythm-Dylan <dperunko@comcast.net>
2024-10-25 17:45:45 -0400

    Physical device name: linux

--- src/main.zig
+++ src/main.zig
@@ -1,14 +1,19 @@
 const std = @import("std");
 const window = @import("./interfacing/window.zig");
 const graphics = @import("./graphics/graphics.zig");
+
 const vkfuncs = @import("./graphics/vulkan/methods.zig");
+const vktypes = @import("./graphics/vulkan/types.zig");
 
 pub fn main() !void {
     const os_window = try window.OSWindow.init(.{});
     os_window.show();
 
     const graphics_instance = try graphics.Graphics.init();
-    std.debug.print("Physical device count: {}\n", .{ try graphics_instance.getPhysicalDeviceCount() });
+
+    var properties: vktypes.VkPhysicalDeviceProperties = undefined;
+    vkfuncs.vkGetPhysicalDeviceProperties(graphics_instance.active_physical_device.*, &properties);
+    std.debug.print("Active physical device name: {s}\n", .{ properties.deviceName });
 
     os_window.loop();
 }
